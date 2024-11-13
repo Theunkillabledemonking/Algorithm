e9de9cf3aeeb545afeff399e7a5d052f443430e0
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2024-11-13 15:47:59 +0900

    SelectionSortAlrorithm

---
+++ dongbinna/java/Selection_Sort.java
@@ -0,0 +1,28 @@
+public class Selection_Sort{
+    // 정렬 알고리즘
+    public static void main(String[] args) {
+        // 다음의 숫자들을 오름차순으로 작성
+        int index = 0, temp = 0, min = 0;
+        int[] array = {1, 10, 5, 8, 7, 5, 4, 3, 2, 9};
+        for (int i = 0; i < 10; i++) {
+            min = 9999; // 무작위로 큰 값
+            for (int j = i; j < 10; j++) {
+                if(min > array[j]) { // min 값보다 array[j]보다 작은 경우
+                    min = array[j]; // 최솟값 갱신
+                    index = j; // 최솟값의 인덱스를 저장
+                }
+            }
+            temp = array[i]; // 현재 위치의 array[i] 값을 임시로 저장
+            array[i] = array[index]; // 최솟값인 arrayh[index]를 array[i] 위치로 배치
+            array[index] = temp; // 기존 i번째 값을 최소값이 있던 위치에 배치
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d", array[i]);
+        }
+    
+    }
+}
+// 1 2 3 4 5 6 7 8 9 10
+// 10 + 9 + 8 + .. + 1 -> 10 * (10 + 1) /2 = 55번을 비교연산을 해야 한다.
+// 수식으로 N * (N + 1) / 2 -> O(N * N) 수행시간을 가지고 있다.
+// 데이터의 양이 조금 많아져도 연산 횟수가 많아지므로 효율적이지는 않다.
